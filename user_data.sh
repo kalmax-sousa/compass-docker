25076c262588d788ca693cf43d9b1d2b82e86946
Kalmax Santos <kalmax.santos.123@gmail.com>
2023-10-30 13:37:43 -0300

    Submissão da Tarefa

---
+++ user_data.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo amazon-linux-extras install docker -y
+sudo service docker start
+sudo systemctl enable docker.service
+sudo usermod -a -G docker ec2-user
+
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+sudo yum install -y amazon-efs-utils
+sudo mkdir /efs
+sudo chmod 777 /efs/
+sudo mount -t efs -o tls fs-09fe1ec767bc01f72:/ /efs
+
+echo "fs-09fe1ec767bc01f72:/ /efs efs _netdev,noresvport,tls,iam 0 0" >> /etc/fstab
+
+export WORDPRESS_DB_USER=admin
+export WORDPRESS_DB_PASSWORD=wordpress
+
+mkdir /myapp
+
+cat << EOF > /myapp/docker-compose.yml
+version: '3.1'
+
+services:
+
+  wordpress:
+    image: wordpress
+    restart: always
+    ports:
+      - 80:80
+    environment:
+      WORDPRESS_DB_HOST: wordpress.c1k5hjsnsxov.us-east-1.rds.amazonaws.com:3306
+      WORDPRESS_DB_USER: $WORDPRESS_DB_USER
+      WORDPRESS_DB_PASSWORD: $WORDPRESS_DB_PASSWORD
+      WORDPRESS_DB_NAME: wordpress
+    volumes:
+      - /efs/wordpress:/var/www/html
+
+  db:
+    image: mysql:5.7
+    restart: always
+    environment:
+      MYSQL_DATABASE: wordpress
+      MYSQL_USER: admin
+      MYSQL_PASSWORD: wordpress
+      MYSQL_RANDOM_ROOT_PASSWORD: '1'
+    volumes:
+      - db:/var/lib/mysql
+
+volumes:
+  wordpress:
+  db:
+EOF
+
+cd /myapp
+
+docker-compose up -d
+cd ..
